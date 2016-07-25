c5fdc272c55cc1555dcb7eab89e766a020f6a46f
edneystrindade <edneystrindade@gmail.com>
2016-07-25 17:42:42 -0300

    Exercício 02
    Desenvolvimento da vídeo aula número 02.

--- Exercício 01/src/org/cbsoft/framework/FileSerializer.java
+++ Exercício 01/src/org/cbsoft/framework/FileSerializer.java
@@ -2,6 +2,9 @@ package org.cbsoft.framework;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import java.util.Map;
 
 public class FileSerializer {
@@ -16,8 +19,8 @@ public class FileSerializer {
 		this.df = df;
 	}
 
-	public void generateFile(String filename, PropertiesGetter propGetter) {
-		byte[] bytes = df.FormatData(propGetter.getPropertiesList());
+	public void generateFile(String filename, Object obj) {
+		byte[] bytes = df.FormatData(getPropertiesList(obj));
 		
 		try {
 			bytes = pp.postProcess(bytes);	
@@ -28,6 +31,35 @@ public class FileSerializer {
 			throw new RuntimeException("Problems writing the file",e);
 		}
 		
+	}
+	
+	private Map<String, Object> getPropertiesList(Object obj) {
+		Map<String,Object> props = new HashMap<String, Object>();
+		Class<?> clazz = obj.getClass();
+		
+		for (Method m: clazz.getMethods()){
+			if (isAllowedGetter(m)) {
+				try {
+					Object value = m.invoke(obj);
+					String getterName = m.getName();
+					String propName = getterName.substring(3).toLowerCase();
+					props.put(propName, value);
+				} catch (Exception e) {
+					throw new RuntimeException("Cannot retrieve properties.", e);
+				} 
+				
+			}
+		}
+		
+		return props;
+	}
+
+	private boolean isAllowedGetter(Method m) {
+		return m.getName().startsWith("get") && 
+				m.getParameterTypes().length == 0 &&
+				m.getReturnType() != void.class &&
+				!m.getName().equals("getClass") &&
+				!m.isAnnotationPresent(DontIncludeOnFile.class);
 	}
 	
 }
